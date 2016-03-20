84c59503a12ff1c8f8ac12cbfe84d4f3551ecacb
Paul Daigle <j.paul.daigle@gmail.com>
2016-03-19 20:00:46 -0400

    syntax error

--- config/test.exs
+++ config/test.exs
@@ -3,7 +3,7 @@ config :loggex, Loggex.Repo,
   adapter: Ecto.Adapters.Postgres,
   pool: Ecto.Adapters.SQL.Sandbox,
   username: "runner",
-  password: "semaphoredb"
+  password: "semaphoredb",
   database: "loggex_test",
   size: 1
 
