f2bb10bd8b86719e57458802c6e3c4adca8ea852
Paul Daigle <j.paul.daigle@gmail.com>
2016-03-20 11:42:09 -0400

    fix typo in test.exs

--- config/test.exs
+++ config/test.exs
@@ -3,7 +3,7 @@ config :loggex, Loggex.Repo,
   adapter: Ecto.Adapters.Postgres,
   pool: Ecto.Adapters.SQL.Sandbox,
   username: "postgres",
-  password: "runner"
+  password: "runner",
   database: "semaphoredb",
   size: 1
 
