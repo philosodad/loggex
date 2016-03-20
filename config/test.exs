c43aa3ede5ed4e6729329c590a5156f5a777cdb9
Paul Daigle <j.paul.daigle@gmail.com>
2016-03-20 17:14:36 -0400

    Add Semaphore Settings

--- config/test.exs
+++ config/test.exs
@@ -2,8 +2,8 @@ use Mix.Config
 config :loggex, Loggex.Repo,
   adapter: Ecto.Adapters.Postgres,
   pool: Ecto.Adapters.SQL.Sandbox,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env["DATABASE_POSTGRESQL_USERNAME"] || "postgres",
+  password: System.get_env["DATABASE_POSTGRESQL_PASSWORD"] || "postgres",
   database: "loggex_test",
   size: 1
 
