bf6d8e1fa6c17bbaad51b704de8861b3e24da178
Paul Daigle <j.paul.daigle@gmail.com>
2016-03-19 20:06:51 -0400

    remove createdb from the travis.yml

--- config/test.exs
+++ config/test.exs
@@ -2,8 +2,7 @@ use Mix.Config
 config :loggex, Loggex.Repo,
   adapter: Ecto.Adapters.Postgres,
   pool: Ecto.Adapters.SQL.Sandbox,
-  username: "runner",
-  password: "semaphoredb",
+  username: "postgres",
   database: "loggex_test",
   size: 1
 
